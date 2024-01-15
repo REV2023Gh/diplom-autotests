dfccb1efa195e80798e43adb65a96dcbb1611cfb
REV2023Gh <153507483+REV2023Gh@users.noreply.github.com>
2024-01-15 23:09:23 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -11,15 +11,14 @@ def post_new_order(body):
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_NEW_ORDER, json=body, headers=data.headers)
 
 
-response = post_new_order(data.order_body)
-print(response.json())
-
-
 #Получение номера заказа
 def get_track_number():
-    #response = post_new_order(data.order_body)
-    track_number = response.json().get('track')
+    response = post_new_order(data.order_body)
+    track_number = response.json()
     return track_number
 
 
-print(get_track_number())
+#Получение заказа
+def get_order_by_track(track_number):
+    get_order = (configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK + f'{track_number}')
+    return requests.get(get_order)
