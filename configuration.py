f343e823cff6520ffb72a6590e46a77f4d076168
REV2023Gh <153507483+REV2023Gh@users.noreply.github.com>
2024-01-16 17:14:00 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
 URL_SERVICE = "https://a408055c-1a60-49c6-9b81-312dbf2c890e.serverhub.praktikum-services.ru"
 DOCS = "/docs/"
 CREATE_NEW_ORDER = "/api/v1/orders"
-GET_ORDER_BY_TRACK = "/api/v1/orders/track?="
+GET_ORDER_BY_TRACK = "/api/v1/orders/track?t="
